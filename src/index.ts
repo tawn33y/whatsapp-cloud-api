9701e4d3539c6268cb4bbc03b95200d1bebe449f
Tony <tawn33y@gmail.com>
2022-05-29 22:14:06 +0300

    ISSUE-1 Export types

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ import { sendMessages } from './sendMessages';
 export const createBot = (fromPhoneNumberId: string, accessToken: string, version: string = 'v14.0') => ({
   ...sendMessages(fromPhoneNumberId, accessToken, version),
 });
+
+export * from './index.types';
---
+++ src/index.types.ts
@@ -0,0 +1,5 @@
+export type { SendMessageResult } from './sendMessages';
+export type {
+  TextMessageOptions, MediaMessageOptions, LocationMessageOptions,
+  ContactMessageOptions, InteractiveMessageOptions, TemplateMessageOptions,
+} from './messages';

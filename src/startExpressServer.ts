3c48864aa2e2ebd08103e7447f30b720b1a62a35
alfonsusac <alfonsus.chendrawan@binus.ac.id>
2022-10-26 18:12:35 +0700

    Implement new path to receive statuses

--- src/startExpressServer.ts
+++ src/startExpressServer.ts
@@ -64,73 +64,113 @@ export const startExpressServer = (
       res.sendStatus(400);
       return;
     }
-    if (req.body?.entry?.[0]?.changes?.[0]?.value?.statuses) {
-      res.sendStatus(202);
+    if (req.body?.entry?.[0]?.changes?.[0]?.value?.statuses?.[0]) {
+      // console.log('statuses_express');
+      const {
+        id,
+        status,
+        timestamp,
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        recipient_id,
+        ...rest
+      } = req.body.entry[0].changes[0].value.statuses[0];
+      let event: PubSubEvent | undefined;
+      let errors: FreeFormObject[] | undefined;
+      let conversation: FreeFormObject | undefined;
+      let pricing: FreeFormObject | undefined;
+      switch (status) {
+        case 'read':
+          event = PubSubEvents.read;
+          break;
+        case 'sent':
+          event = PubSubEvents.sent;
+          conversation = rest.conversation;
+          pricing = rest.pricing;
+          break;
+        case 'delivered':
+          event = PubSubEvents.delivered;
+          conversation = rest.conversation;
+          pricing = rest.pricing;
+          break;
+        case 'failed':
+          event = PubSubEvents.failed;
+          errors = rest.errors;
+          break;
+        default:
+          break;
+      }
+      if (event) {
+        let payload:FreeFormObject = {
+          id,
+          status,
+          timestamp,
+          recipient_id,
+        };
+        if (errors) { payload = { ...payload, errors }; }
+        if (conversation) { payload = { ...payload, conversation }; }
+        if (pricing) { payload = { ...payload, pricing }; }
+        ['status', event].forEach((e) => PubSub.publish(e, payload));
+      }
+      res.sendStatus(200);
       return;
     }
-
-    const {
-      from,
-      id,
-      timestamp,
-      type,
-      ...rest
-    } = req.body.entry[0].changes[0].value.messages[0];
-
-    let event: PubSubEvent | undefined;
-    let data: FreeFormObject | undefined;
-
-    switch (type) {
-      case 'text':
-        event = PubSubEvents.text;
-        data = { text: rest.text?.body };
-        break;
-
-      case 'image':
-      case 'document':
-      case 'audio':
-      case 'video':
-      case 'sticker':
-      case 'location':
-      case 'contacts':
-        event = PubSubEvents[type as PubSubEvent];
-        data = rest[type];
-        break;
-
-      case 'interactive':
-        event = rest.interactive.type;
-        data = {
-          ...(rest.interactive.list_reply || rest.interactive.button_reply),
-        };
-        break;
-
-      default:
-        break;
-    }
-
-    if (rest.context) {
-      data = {
-        ...data,
-        context: rest.context,
-      };
-    }
-
-    const name = req.body.entry[0].changes[0].value.contacts?.[0]?.profile?.name ?? undefined;
-
-    if (event && data) {
-      const payload: Message = {
+    if (req.body?.entry?.[0]?.changes?.[0]?.value?.messages) {
+      console.log('messages_express');
+      const {
         from,
-        name,
         id,
         timestamp,
-        type: event,
-        data,
-      };
-
-      ['message', event].forEach((e) => PubSub.publish(e, payload));
+        type,
+        ...rest
+      } = req.body.entry[0].changes[0].value.messages[0];
+      let event: PubSubEvent | undefined;
+      let data: FreeFormObject | undefined;
+      switch (type) {
+        case 'text':
+          event = PubSubEvents.text;
+          data = { text: rest.text?.body };
+          break;
+        case 'image':
+        case 'document':
+        case 'audio':
+        case 'video':
+        case 'sticker':
+        case 'location':
+        case 'contacts':
+          event = PubSubEvents[type as PubSubEvent];
+          data = rest[type];
+          break;
+        case 'interactive':
+          event = rest.interactive.type;
+          data = {
+            ...(rest.interactive.list_reply || rest.interactive.button_reply),
+          };
+          break;
+        default:
+          break;
+      }
+      if (rest.context) {
+        data = {
+          ...data,
+          context: rest.context,
+        };
+      }
+      const name = req.body.entry[0].changes[0].value.contacts?.[0]?.profile?.name ?? undefined;
+      if (event && data) {
+        const payload: Message = {
+          from,
+          name,
+          id,
+          timestamp,
+          type: event,
+          data,
+        };
+        ['message', event].forEach((e) => PubSub.publish(e, payload));
+      }
+      res.sendStatus(200);
+      return;
     }
-
-    res.sendStatus(200);
+    res.sendStatus(400);
   });
 
   if (options?.app) {
